09444f0b419712de3b24906909e8b5ed66c289b7
RECamp88 <recamp88@gmail.com>
2023-08-07 12:51:46 -0500

    Added Login Service methods for registering a new login and logging in an existing user

--- src/main/java/Repository/LoginRepository.java
+++ src/main/java/Repository/LoginRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LoginRepository extends JpaRepository<Login, Long> {
 
+     public Login findByEmail(String email);
 }
